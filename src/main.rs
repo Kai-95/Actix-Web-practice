d94dd319ffdb311a991cae2a7a6cac0d1d889f0f
kai-95echo <kagorilla9@gmail.com>
2024-03-26 21:51:53 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::io::Result;
+use actix_web::{get, App, HttpMessage, HttpRequest, HttpResponse, HttpServer, Responder ,middleware::Logger};
+use env_logger::Env;
+
+mod handler;
+
+#[get("/")]
+async fn index()-> impl Responder{
+    HttpResponse::Ok().body("hel world")
+}
+
+#[actix_rt::main]
+async fn main()-> Result<()>{
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+    HttpServer::new(||{
+        App::new()
+        .service(handler::index)
+        .wrap(Logger::default())
+    })
+    .bind("127.0.0.1:8000")?.run().await
+}
